be8ff8103a77ccc78b61334fd89a22620e93d14b
IvoVM <ivomastrangelo12@gmail.com>
2023-04-05 03:37:00 -0300

    Interceptor service now use token in session storage

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InterceptorService implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
 
-  constructor(
-    private authenticationService: AuthenticationService
-  ) {}
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const user: any = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')!);
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let mytoken: any = sessionStorage.getItem('token');
     let request = req;
 
-    if (user) {
+    if (mytoken) {
+      mytoken = JSON.parse(mytoken);
       request = req.clone({
         setHeaders: {
-          authorization: `Bearer ${ user.token }`
-        }
+          authorization: `Bearer ${mytoken}`,
+        },
       });
     }
 
     return next.handle(request);
   }
-
 }
