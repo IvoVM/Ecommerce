11cbb65d6f984cf847f841c51afea4c4bc3ba5b8
IvoVM <ivomastrangelo12@gmail.com>
2023-04-11 07:41:58 -0300

    Deleted cards service, getAll cards function now is connected to the server

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,34 +1,36 @@
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Injectable ,Output,EventEmitter} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 interface defaultResponse {
-  data: any,
-  message: string,
-  status: number
+  data: any;
+  message: string;
+  status: number;
 }
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-  constructor(private http:HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
 
-  getAll(page: number): Observable<any> {
-    return this.http.get<defaultResponse>(`${environment.apiUrl}api/products/${page}`).pipe(map((res: defaultResponse) => res.data))
-}
-
-  create(body: {title:string,price:number,description: string, photo:string}): Observable<any> {
-    return this.http.post(`${environment.apiUrl}api/products/`, body);
+  getAll(): Observable<any> {
+    return this.http.get<defaultResponse>(`${environment.apiUrl}products`);
   }
-  delete(id:string): Observable<any> {
-    return this.http.delete<defaultResponse>(`${environment.apiUrl}api/products/${id}`);
+
+  create(body: {
+    title: string;
+    price: number;
+    description: string;
+    img: string;
+    categorie: string;
+  }): Observable<any> {
+    return this.http.post(`${environment.apiUrl}products`, body);
   }
-  
-
-
+  delete(id: string): Observable<any> {
+    return this.http.delete<defaultResponse>(
+      `${environment.apiUrl}api/products/${id}`
+    );
+  }
 }
