b3bb9dc9fb00954ad3fa7f16bfdf51604900539e
IvoVM <ivomastrangelo12@gmail.com>
2023-04-22 08:19:30 -0300

    Applied typescript,  created update function

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,23 +1,34 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export interface User  {
+  username: string;
+  password: string;
+  img?: string;
+  description?: string;
+  _id?: string;
+};
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  login(body: any): Observable<any> {
+  login(body: User): Observable<any> {
     return this.http.post(environment.apiUrl + 'signin', body);
   }
 
-  register(body: { username: string; password: string }): Observable<any> {
+  register(body: User): Observable<any> {
     return this.http.post(environment.apiUrl + 'signup', body);
   }
 
   getLoggedInUser(): Observable<any> {
     return this.http.get(environment.apiUrl + 'me');
+  }
+
+  update(body: User): Observable<any> {
+    return this.http.put(environment.apiUrl + 'me', body);
   }
 }
