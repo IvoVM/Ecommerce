c485b7c3c2f879369fb994b8cf1f320c8fcd5342
IvoVM <ivomastrangelo12@gmail.com>
2023-04-22 08:26:38 -0300

    Implemented spinner, added user profileIMG and price

--- src/app/views/card/card.component.ts
+++ src/app/views/card/card.component.ts
@@ -10,18 +10,24 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class CardComponent implements OnInit {
   @Input() data?: any;
   cart: any[] = [];
+  load = true;
 
   constructor(
     private CartService: AddCartService,
     private _snackBar: MatSnackBar
-  ) {}
+  ) {
+    
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.load = false;
+    }, 1000);
+  }
   public addToCart(data: any) {
     this.CartService.addToCart(data);
     this.openSnackBar(data.title);
   }
-  
 
   public openSnackBar(name: string) {
     this._snackBar.open(`El producto ${name} fue agregado al carro`, 'Cerrar', {
